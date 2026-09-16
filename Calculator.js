fb605b8ae51c3f32b8a3bea46bec0a7f9ee01d5f
Somya142007 <somya142007@gmail.com>
2026-09-16 21:02:51 +0530

    Calculator

---
+++ Calculator.js
@@ -0,0 +1,81 @@
+// function calculator(num1, operator, num2) {
+//     return num1 + num2;
+// }
+// let result=0;
+// switch (operation) {
+//     case 'add':
+//         result = calculator(num1, num2);
+//         break;
+//     case 'subtract':
+//         result = num1 - num2;
+//         break;  
+//     case 'multiply':
+//         result = num1 * num2;
+//         break;
+//     case 'divide':
+//         if (num2 !== 0) {
+//             result = num1 / num2;
+//         } else {
+//             console.log("Error: Division by zero is not allowed.");
+//         }
+//         break;
+//     default:
+//         console.log("Error: Invalid operation.");
+// }
+
+
+
+
+
+
+
+//  console.log("Start")
+//  setTimeout(() => {
+//      console.log("Start setTimeout")
+//  }, 1000);
+//  Promise.resolve("Promise resolved").then((message) => {
+//      console.log(Promise);
+//  })
+//  console.log("End")
+
+
+
+
+
+
+
+
+// import os from 'os'
+// console.log("CPU Architecture:", os.arch());
+// console.log("Model:", os.cpus()[0].model);
+// console.log("Number of Cores:", os.cpus().length);
+// console.log("Total Memory:", os.totalmem());
+// console.log("Free Memory:", os.freemem());
+// console.log("OS Platform:", os.platform());
+// console.log("OS Release:", os.release());
+// console.log("OS Type:", os.type());
+// console.log("OS Uptime:", os.uptime());
+// console.log("Total Memory:", (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2) + " GB");
+// console.log("Free Memory:", (os.freemem() / (1024 * 1024 * 1024)).toFixed(2) + " GB");
+// console.log("Number of cores:", os.cpus().length);
+// console.log("CPU Model:", os.cpus()[0].model);
+
+
+
+
+
+
+
+
+
+
+// function calculate_area (radius){
+//     return 3.14 * radius * radius;  
+// }
+// function calculate_perimeter(radius){
+//     return 2 * 3.14 * radius;
+// }
+
+// console.log("Area of circle with radius 1:", calculate_area(1));
+// console.log("Perimeter of circle with radius 1:", calculate_perimeter(1));
+// module.exports = { calculate_area, calculate_perimeter };

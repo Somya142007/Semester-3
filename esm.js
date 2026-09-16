d85a78a0319a2a71e5b863478ae0e3f96d8ac699
Somya142007 <somya142007@gmail.com>
2026-09-16 21:06:28 +0530

    EMS File

---
+++ esm.js
@@ -0,0 +1,7 @@
+export function isCasteVote(age) {
+    if (age >= 18 ) {
+        console.log("You are eligible to caste vote");
+    } else {
+        console.log("You are not eligible to caste vote");
+    }
+}
